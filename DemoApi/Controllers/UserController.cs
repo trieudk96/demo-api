70b929db61b61303af0fc3ba82953677dc804533
dktrieu <dktrieu@cmc.com.vn>
2019-10-09 16:37:38 +0700

    hihi

--- DemoApi/Controllers/UserController.cs
+++ DemoApi/Controllers/UserController.cs
@@ -31,14 +31,14 @@ namespace DemoApi.Controllers
             var startIndex = pageSize * (pageIndex - 1);
             var segments = startIndex + pageSize < total ? pageSize : total - startIndex;
             var data =
-            _context.Users.Where(s => string.IsNullOrEmpty(searchString) || (
-                !string.IsNullOrEmpty(s.UserName) && s.UserName.Contains(searchString) ||
-                !string.IsNullOrEmpty(s.Name) && s.Name.Contains(searchString) ||
-                s.Age.ToString().Contains(searchString) ||
-                !string.IsNullOrEmpty(s.Gender) && s.Gender.Contains(searchString)))
-                .Skip(startIndex).Take(segments).ToList();
-            res.payload = data;
-            res.total_count = data.Count;
+                _context.Users.Where(s => string.IsNullOrEmpty(searchString) || (
+                                              !string.IsNullOrEmpty(s.UserName) && s.UserName.Contains(searchString) ||
+                                              !string.IsNullOrEmpty(s.Name) && s.Name.Contains(searchString) ||
+                                              s.Age.ToString().Contains(searchString) ||
+                                              !string.IsNullOrEmpty(s.Gender) && s.Gender.Contains(searchString)));
+                
+            res.payload = data.Skip(startIndex).Take(segments).ToList(); ;
+            res.total_count = data.Count();
             return res;
         }
         //[HttpGet]
